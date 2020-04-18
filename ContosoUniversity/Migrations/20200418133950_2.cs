5aaa78e91d7b15c47e261a2a11ad2590abc95d3a
洁 沈 <same114818@hotmail.com>
2020-04-18 21:52:26 +0800

    Revert "Revert "111111""
    This reverts commit cd75eadd80f26dc8980aa01fa51103978df16c15.

---
+++ ContosoUniversity/Migrations/20200418133950_2.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ContosoUniversity.Migrations
+{
+    public partial class _2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ConfirmPassword",
+                table: "User");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ConfirmPassword",
+                table: "User",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true);
+        }
+    }
+}
