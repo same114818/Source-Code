fc25f73655975a3d47b4ee5c934931c8b53f8412
洁 沈 <same114818@hotmail.com>
2020-04-18 18:50:40 +0800

    123

--- ContosoUniversity/Controllers/UserController.cs
+++ ContosoUniversity/Controllers/UserController.cs
@@ -28,7 +28,7 @@ namespace ContosoUniversity.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Login(User user)
         {
-            if (ModelState.IsValid)
+            if (ModelState.IsValid)//test
             {
                 var info = _context.User.FirstOrDefault(o => o.UserName == user.UserName && o.Password == user.Password && o.Status == 1);
                 if (info!=null)
